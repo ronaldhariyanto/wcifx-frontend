2838ce8938942e84ae3594e56647c87100f1d198
Ronald Hariyanto <ronaldhariyanto@gmail.com>
2016-09-25 16:31:17 +0700

    DAY#05: Updating homepage

--- assets/scripts/application.js
+++ assets/scripts/application.js
@@ -1,19 +1,30 @@
-$('.slider-homepage').slick();
+$('.parallax-window').parallax({imageSrc: 'images/bg-parrallax-01.jpg'});
 
-$('.slider-services').slick({
-  infinite: true,
-  slidesToShow: 4,
-  slidesToScroll: 1
+$('.our-services--homepage').parallax({
+  imageSrc: 'images/slide-home-bg2.jpg',
+  naturalWidth: '1440px'
 });
-
-$('.news-slider').slick();
-
-$('#scene').parallax();
 
 $(document).ready(function() {
   $('.select2').select2({
     minimumResultsForSearch: Infinity
   });
+
+  $('.slider-homepage').slick();
+
+  $('.slider-nav--action li a').click(function(e){
+    e.preventDefault();
+    var slideIndex = $('.slider-nav--action li a').index(this);
+    $('.slider-homepage').slick('slickGoTo', parseInt(slideIndex));
+  });
+
+  $('.slider-services').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1
+  });
+
+  $('.news-slider').slick();
 
   $('.diary-news').masonry({
     itemSelector: '.news--items',
