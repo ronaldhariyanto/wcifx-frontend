526b4c1004c1e718b6453ee18e2b9686406afe21
ronaldmoneysmart <ronald.hariyanto@loangarage.com>
2016-10-03 03:22:43 +0700

    DAY#13: Added Businnes web pages.

--- assets/scripts/application.js
+++ assets/scripts/application.js
@@ -1,3 +1,5 @@
+'use strict';
+
 $('.parallax-window--diary').parallax({ imageSrc: 'images/bg-parrallax-01.jpg' });
 
 $('.parallax-window--news').parallax({ imageSrc: 'images/bg-news.jpg' });
@@ -7,6 +9,8 @@ $('.parallax-window--about').parallax({ imageSrc: 'images/bg-about.jpg' });
 $('.parallax-window--contest').parallax({ imageSrc: 'images/bg-contest.jpg' });
 
 $('.parallax-contest').parallax({ imageSrc: 'images/bg-contest-03.jpg' });
+
+$('.parallax-window--businnes').parallax({ imageSrc: 'images/bg-about-02.jpg' });
 
 $('.our-services--homepage').parallax({
   imageSrc: 'images/slide-home-bg2.jpg',
@@ -53,3 +57,42 @@ $(document).ready(function() {
   });
 
 });
+
+// http://gionkunz.github.io/chartist-js/api-documentation.html
+
+var data = {
+  labels: ['1977', '1987', '1992', '1997', '2000', '2006', '2010', '2015'],
+  series: [
+    [
+      10, 15, 20, 25, 27, 25, 23, 25
+    ],
+    [2, 3, 5, 5, 10, 12, 15, 17]
+  ]
+};
+
+var options = {
+  seriesBarDistance: 21,
+  height: 580,
+  axisY: {
+    type: Chartist.FixedScaleAxis,
+    low: 0,
+    high: 30,
+    ticks: [0, 5, 10, 15, 20, 25, 30],
+    labelInterpolationFnc: function(value) {
+      return Math.floor(value);
+    }
+  }
+};
+
+var responsiveOptions = [
+  ['screen and (max-width: 640px)', {
+    seriesBarDistance: 15,
+    axisX: {
+      labelInterpolationFnc: function (value) {
+        return value[0];
+      }
+    }
+  }]
+];
+
+new Chartist.Bar('.ct-chart', data, options, responsiveOptions);
