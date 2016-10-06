bcd46fc0670b7cdfcab24970291f1f98b64aea23
Ronald Hariyanto <ronaldhariyanto@gmail.com>
2016-10-06 09:13:29 +0700

    DAY#16: [WIP] Trading pages

--- assets/scripts/application.js
+++ assets/scripts/application.js
@@ -14,6 +14,8 @@ $('.parallax-window--businnes').parallax({ imageSrc: 'images/bg-about-02.jpg' })
 
 $('.parallax-window--partnership').parallax({ imageSrc: 'images/bg-partnership.jpg' });
 
+$('.parallax-window--trading-real').parallax({ imageSrc: 'images/bg-trading-02.jpg' });
+
 $('.our-services--homepage').parallax({
   imageSrc: 'images/slide-home-bg2.jpg',
   naturalWidth: '1440px'
